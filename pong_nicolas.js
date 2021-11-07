eea1cb85db683fba8556c953d5d0f85974d8bfb7
nicolash1989 <91009364+nicolash1989@users.noreply.github.com>
2021-11-07 19:51:33 +0100

    commit5

--- pong_nicolas.js
+++ pong_nicolas.js
@@ -1,111 +1,68 @@
 
+let x
+let y
+let t = 0
+let r
+let alpha = 255
+let circleSize = 5
+let tracesSize = 5
+let tracesArray = []
 
-//ball
-let ballD = 20
-let ballCount = 2
-let speedX = 6
-let speedY = 6
+let lissy
+let lissy2
 
-//traces
-let traceLen = 20
-let traceDia = 20
-
-//board
-let myBoard
-let boardGap = 100
-let boardSizeX = 150
-let boardSizeY = 12
-
-//arrays
-let ballPos = []
-let traces = []
-
-//class board
-class Board {
-    constructor() {
-        this.boardX = mouseX
-        this.boardY = boardGap
+class Lissajous {
+    constructor(r) {
+        this.t = t
+        this.x = x
+        this.y = y
+        this.r = r
+        this.alpha = alpha
     }
 
-    newBoard() {
-        rect(this.boardX - boardSizeX/2, windowHeight - boardGap - boardSizeY, boardSizeX, boardSizeY)
-    }
-    updateBoard() {
-        this.boardX = mouseX
-    }
-}
-
-//class balls
-class Ball {
-    constructor() {
-        this.ballX = random(20, width)
-        this.ballY = random(20, height/2)
-        this.speedX = speedX
-        this.speedY = speedY
-    }
-    
-    drawBall() {
-        ellipse(this.ballX, this.ballY, ballD)
-    }
-    checkPosition() {
-        if(this.ballX - ballD/2  < 0 || this.ballX + ballD/2 > windowWidth) {
-        this.speedX *= -1
-        }
-        if(this.ballY - ballD/2 < 0 || this.ballY + ballD/2 > windowHeight) {
-        this.speedY *= -1
+    saveTraces() {
+        tracesArray.push({x: this.x, y: this.y, t: this.t})
+        if(tracesArray.length > 100) {
+            tracesArray.shift()
         }
     }
-    updatePosition() {
-        this.ballX += this.speedX
-        this.ballY += this.speedY
+
+    drawCircle() {
+        fill(255, 255, 255);
+        ellipse(this.x, this.y, circleSize, circleSize)
     }
-    pushTraces() {
-        traces.push({x: this.ballX, y: this.ballY})
-        if (traces.length > traceLen){
-            traces.shift()
-        }
-    }
+
     drawTraces() {
-        for (let j = 0; j < traces.length; j += 1){
-            ellipse(traces[j].x + random(-3,3), traces[j].y + random(1,3), traceDia)
-        }   
+        for (let i = 0; i < tracesArray.length; i++) {
+            fill(255,255,255,this.alpha)
+            ellipse(tracesArray[i].x, tracesArray[i].y, tracesSize, tracesSize)
+        }
+    }
+
+    update(r) {
+        this.x = width / 2 + this.r * sin(3 * this.t + PI / 2)
+        this.y = height / 2 + this.r * sin(this.t)
+        this.t += .01
     }
 }
-
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
+    createCanvas(500, 500);
     
-    for (let i = 0; i < ballCount; i += 1) {
-        ballPos.push(new Ball())
-    }
-    
-    myBoard = new Board
+    lissy = new Lissajous(200)
+    lissy2 = new Lissajous(150)
 }
-  
 
 function draw() {
+    background(0);
 
-    //hintergrund
-    background(255)
+    lissy.saveTraces()
+    lissy.update()
+    lissy.drawCircle()
+    lissy.drawTraces()
 
-    //loop und methoden balls
-    for (let i = 0; i < ballPos.length; i += 1) {
-        ballPos[i].pushTraces()
-        ballPos[i].drawTraces()
-        ballPos[i].checkPosition()
-        ballPos[i].updatePosition()
-        //ballPos[i].drawBall()
-    }
-
-    //methoden board
-    myBoard.newBoard()
-    myBoard.updateBoard()
-    
-
-
-    //board_richtungswechsel
-    //if(ballY > windowHeight - boardGap - boardSizeY - ballD/2 && ballX > mouseX - boardSizeX/2 && ballX < mouseX + boardSizeX/2){
-    //    speedY = -speedY
-    //}
+    lissy2.saveTraces()
+    lissy2.update()
+    lissy2.drawCircle()
+    lissy2.drawTraces()
 }
